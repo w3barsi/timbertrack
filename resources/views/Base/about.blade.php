9d88579a38b99555ba737c5a54f3d5c07275b67d
robinehoodie <74012464+robinehoodie@users.noreply.github.com>
2020-12-29 01:50:34 +0800

    about page

--- resources/views/Base/about.blade.php
+++ resources/views/Base/about.blade.php
@@ -3,12 +3,18 @@
 @section('body')
         <div id="about" class="fade-in1">
             <p style="font-size: 100px; color:black; margin-top: 2%; margin-bottom: 0%;">ABOUT US</p>
-            <p style="font-size: 24px; color:white; margin-top: 0%; margin-bottom: 0%;">we like wood<br>we eat wood<br>we buy wood<br>we sell wood</p>
+            <p style="font-size: 24px; color:white; margin-top: 0%; margin-bottom: 0%;">Tony’s lumber has been operating for over 10 years
+                and has given service to a variety of
+                customers commonly <br>the people in Danao. We enjoy the custom of conversing with other people and providing
+                them with their needs <br>for shelter and quality living. With the skills and experiences that were gained from
+                the founder of Tony’s Lumber,<br> we were able to establish a business that is able to offer construction materials
+                for people to build their houses <br>and other structures. You can start building your dreams at Tony’s Lumber and
+                Construction Supply.</p>
 
             <p style="font-size: 60px; color:black; margin-top: 5%; margin-bottom: 0%;">what drives us</p>
             <p style="font-size: 24px; color:white; margin-top: 0%; margin-bottom: 0%;">our team decided to create a management mechanism for a lumber business</p>
             <p style="font-size: 60px; color:black; margin-top: 2%; margin-bottom: 0%;">who we are</p>
             <p style="font-size: 24px; color:white; margin-top: 0%; margin-bottom: 0%;">we are students FORCED to make a website ./.</p>
         </div>
- 
+
 @endsection

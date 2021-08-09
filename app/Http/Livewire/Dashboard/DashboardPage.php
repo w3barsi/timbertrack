9afa4ec28d57eb213aaf47ac22c3b59ade8bb456
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-08-10 05:36:30 +0800

    updated dashboard
    updated dashboard

--- app/Http/Livewire/Dashboard/DashboardPage.php
+++ app/Http/Livewire/Dashboard/DashboardPage.php
@@ -4,21 +4,44 @@ namespace App\Http\Livewire\Dashboard;
 
 use App\Models\Stock;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class DashboardPage extends Component
 {
+    use WithPagination;
 
-    public $Stocks;
+    public $selected;
+
+    public $stocks;
+
+    public function selected($selected)
+    {
+
+        $this->selected = $selected;
+
+        if ($this->selected === 'others') {
+            $this->stocks = Stock::where([
+                ['category', '!=', 'wood'],
+                ['category', '!=', 'plastic'],
+                ['category', '!=', 'metal'],
+                ['category', '!=', 'concrete'],
+            ])->orderBy('created_at', 'desc')->get();
+        } else {
+            $this->stocks = Stock::where('category', $selected)->orderBy('created_at', 'desc')->get();
+        }
+    }
 
     public function mount()
     {
-        $this->Stocks = Stock::orderBy('created_at', 'desc')->get();
+        $this->selected="";
+        $this->stocks = Stock::where('category',$this->selected)->orderBy('created_at', 'desc')->get();
+
     }
 
     public function render()
     {
         return view('livewire.dashboard.dashboard-page')
             ->extends('layouts.app')
-            ->section('body');;
+            ->section('body');
     }
 }

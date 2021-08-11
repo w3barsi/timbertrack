8c415b66c3245e391589f6efd9afba72df7f8b8a
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-08-11 16:04:11 +0800

    dashboard bottom right table
    dashboard bottom right table

--- app/Http/Livewire/Dashboard/DashboardPage.php
+++ app/Http/Livewire/Dashboard/DashboardPage.php
@@ -3,9 +3,12 @@
 namespace App\Http\Livewire\Dashboard;
 
 use App\Models\Stock;
+use App\Models\Purchase;
+use App\Models\Order;
 use Livewire\Component;
 use Livewire\WithPagination;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardPage extends Component
 {
@@ -13,14 +16,24 @@ class DashboardPage extends Component
 
     public $selected;
     public $stocks;
+
+    ////////////////DATE
     public $date;
     public $day ;
     public $month ;
     public $year ;
     public $weekname;
     public $today;
+    public $chosenDate;
+    /////////////////////
+
+    ////////////PURCHASED
+    public $purchased;
+    public $pID;
+    ////////////////////
 
     public function updatedDate(){
+        $this->chosenDate = $this->date;
         $this->day = date("d",strtotime($this->date));
         $this->weekname = date("l",strtotime($this->date));
         $this->month = date("F",strtotime($this->date));
@@ -42,15 +55,30 @@ class DashboardPage extends Component
         }
     }
 
+    public function purchases($id){
+
+        $this->pID = $id;
+        $this->purchased = Purchase::where('stock_id',$id)
+        ->whereDate('created_at', '=', $this->chosenDate)
+        ->get();
+    }
+
     public function mount()
     {
         $this->selected="";
         $this->today=Carbon::now();
+        $this->chosenDate  = $this->today;
+
         $this->day=date("d",strtotime($this->today));
         $this->weekname=date("l",strtotime($this->today));
         $this->month=date("F",strtotime($this->today));
         $this->year=date("y",strtotime($this->today)) + 2000;
         $this->stocks = Stock::where('category',$this->selected)->orderBy('created_at', 'desc')->get();
+
+        $this->pID = "";
+        $this->purchased = Purchase::where('stock_id',$this->pID)
+        ->whereDate('created_at', '=', $this->chosenDate)
+        ->get();
     }
 
     public function render()

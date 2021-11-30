e05d86b513f05826f38ed4ebeeeedbbbb938f04b
z3n5hif7 <37139511+z3n5hif7@users.noreply.github.com>
2021-11-30 16:33:51 +0800

    Roles: Admin, Supplier, Employee
    Roles: Admin, Supplier, Employee

--- resources/views/livewire/components/view-registered-row.blade.php
+++ resources/views/livewire/components/view-registered-row.blade.php
@@ -16,20 +16,13 @@
                 <option value="{{$user->position}}">{{$user->position}}</option>
                 @if($user->position == 'Admin')
                 <option value="Employee">Employee</option>
-                <option value="Checker">Checker</option>
-                <option value="Cashier">Cashier</option>
-                @elseif($user->position == 'Cashier')
-                <option value="Admin">Admin</option>
-                <option value="Checker">Checker</option>
-                <option value="Employee">Employee</option>
-                @elseif($user->position == 'Checker')
-                <option value="Admin">Admin</option>
-                <option value="Cashier">Cashier</option>
-                <option value="Employee">Employee</option>
+                <option value="Supplier">Supplier</option>
                 @elseif($user->position == 'Employee')
                 <option value="Admin">Admin</option>
-                <option value="Checker">Checker</option>
-                <option value="Cashier">Cashier</option>
+                <option value="Supplier">Supplier</option>
+                @else($user->position == 'Supplier')
+                <option value="Admin">Admin</option>
+                <option value="Employee">Employee</option>
                 @endif
             </select>
         </td>
